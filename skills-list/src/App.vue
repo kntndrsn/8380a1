6cc27337d287992b8edc5673a435d64a523d83eb
Kent Anderson <kntndrsn@gmail.com>
2021-01-31 20:59:39 -0600

    Fixed some styling issues

--- skills-list/src/App.vue
+++ skills-list/src/App.vue
@@ -14,73 +14,23 @@ export default {
 };
 </script>
 
-<style scoped>
-@import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
-@import "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";
+<style>
+@import url("https://fonts.googleapis.com/css?family=Montserrat:400,700");
 
-.holder {
-  background: #fff;
+body {
+  background-color: #eeeeee;
+  font-family: "Montserrat", sans-serif;
+  display: grid;
+  grid-template-rows: auto;
+  justify-items: center;
+  align-items: center;
 }
-
-ul {
+body,
+html {
   margin: 0;
-  padding: 0;
-  list-style-type: none;
+  height: 100%;
 }
-
-ul li {
-  padding: 20px;
-  font-size: 1.3em;
-  background-color: #e0edf4;
-  border-left: 5px solid #3eb3f6;
-  margin-bottom: 2px;
-  color: #3e5252;
-}
-
-p {
-  text-align: center;
-  padding: 30px 0;
-  color: gray;
-}
-
-.container {
-  box-shadow: 0px 0px 40px lightgray;
-}
-
-input {
-  width: calc(100% - 40px);
-  border: 0;
-  padding: 20px;
-  font-size: 1.3em;
-  background-color: #323333;
-  color: #687f7f;
-}
-.alert {
-  background: #fdf2ce;
-  font-weight: bold;
-  display: inline-block;
-  padding: 5px;
-  margin-top: -20px;
-}
-.alert-in-enter-active {
-  animation: bounce-in 0.5s;
-}
-.alert-in-leave-active {
-  animation: bounce-in 0.5s reverse;
-}
-@keyframes bounce-in {
-  0% {
-    transform: scale(0);
-  }
-  50% {
-    transform: scale(1.5);
-  }
-  100% {
-    transform: scale(1);
-  }
-}
-i {
-  float: right;
-  cursor: pointer;
+#app {
+  width: 50%;
 }
 </style>
